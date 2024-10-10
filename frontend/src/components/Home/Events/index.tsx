8d1be6c6ceb23675e280dec19a9b587d20e9d224
Anderson Osayerie <33299704+andemosa@users.noreply.github.com>
2024-10-10 23:09:09 +0100

    Andemosa/hero (#9)
    Homepage

--- frontend/src/components/Home/Events/index.tsx
+++ frontend/src/components/Home/Events/index.tsx
@@ -2,9 +2,9 @@ import EventCard from "./EventCard"
 
 const UpcomingEvents = () => {
   return (
-    <section className="font-poppins space-y-6 md:space-y-8 lg:space-y-10 py-10 md:py-14 lg:py-20">
-      <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold text-center">Upcoming Events</h2>
-      <div className="mx-auto grid gap-3 items-start grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+    <section className="font-poppins space-y-6 md:space-y-8 lg:space-y-10 py-10 md:py-14 lg:py-24 lg:pb-32">
+      <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold text-center">Upcoming Events</h2>
+      <div className="mx-auto grid gap-3 sm:gap-6 items-start grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
         {[1, 2, 3, 4].map(item => <EventCard key={item} />)}
       </div>
     </section>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,17 +1,19 @@
-import Community from "@/components/Home/Community"
-import UpcomingEvents from "@/components/Home/Events"
-import Hero from "@/components/Home/Hero"
-import Works from "@/components/Home/Works"
+import Community from "@/components/Home/Community";
+import UpcomingEvents from "@/components/Home/Events";
+import Hero from "@/components/Home/Hero";
+import Works from "@/components/Home/Works";
 
 const HomePage = () => {
   return (
-    <div className='px-4 sm:px-6 lg:px-8'>
+    <>
       <Hero />
-      <UpcomingEvents />
-      <Works />
-      <Community />
-    </div>
-  )
-}
+      <div className="px-4 sm:px-6 lg:px-8">
+        <UpcomingEvents />
+        <Works />
+        <Community />
+      </div>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
